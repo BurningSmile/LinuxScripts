d6e63f748d32bc29cd83d32f2ccee169b9598d21
BurningSmile <acdcman200@gmail.com>
2017-01-15 15:48:55 -0700

    added installi3.sh

---
+++ Arch/installi3.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Get sudo for script run
+sudo -v
+
+#Backup .Xresources
+sudo cp ~/.Xresources ~/.Xresources.bak
+
+# Install required packages from main Repos
+sudo pacman -S --needed i3 rofi feh lxappearance compton git wget unzip --noconfirm
+yes | sudo pacman -S termite
+
+#install i3blocks from AUR
+cd ~/Downloads
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/i3blocks.tar.gz
+tar -xvf i3blocks.tar.gz
+cd i3blocks
+makepkg -si --noconfirm
+
+#install fonts for system
+mkdir ~/.fonts
+cd ~/Downloads
+wget https://github.com/chrissimpkins/Hack/releases/download/v2.020/Hack-v2_020-ttf.zip
+mkdir HackFont
+unzip Hack-v2_020-ttf.zip -d HackFont
+cd HackFont
+cp Hack-Regular.ttf ~/.fonts
+
+cd ~/Downloads
+wget https://github.com/FortAwesome/Font-Awesome/archive/v4.7.0.tar.gz
+tar -xvzf v4.7.0.tar.gz
+cd  'Font-Awesome-4.7.0/fonts'
+cp fontawesome-webfont.ttf ~/.fonts
+
+#Clean up Downloads folder
+cd ~/Downloads
+rm -rf Font-Awesome-4.7.0 HackFont i3blocks
+rm Hack-v2_020-ttf.zip  v4.7.0.tar.gz i3blocks.tar.gz
+
+#Download config files from GitHub
+cd ~
+git clone https://github.com/BurningSmile/LinuxConfigs.git
+cd ~/LinuxConfigs/i3
+
+#Install configs
+mkdir ~/.config/i3
+cp config ~/.config/i3/
+cp i3blocks.conf ~/.config/i3/
+cp -r scripts ~/.config/i3
+cp .Xresources ~
+cd Termite_Terminal
+sudo mkdir ~/.config/termite/
+sudo cp config ~/.config/termite/
+
+#Copy background image
+cp ~/LinuxConfigs/i3/Background/wallpaperArch.png ~/Pictures
+
+cd ~
+#Echo when complete
+echo "Install is finished. Restart Arch Linux to log into i3."
