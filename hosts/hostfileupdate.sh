4a705c60087b75f4808e520c661ff1d5f5e23a57
BurningSmile <burningsmile10@gmail.com>
2017-04-17 21:36:52 -0600

    Added git stash save --keep-index to prevent errors on git pull

--- hosts/hostfileupdate.sh
+++ hosts/hostfileupdate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #This script is ment to be ran by a cronjob. Please change USERNAMEHERE with your username.
 cd /home/USERNAMEHERE/.scripts/hosts/
+git stash save --keep-index
 git pull #update repo
 python3 updateHostsFile.py --auto --replace  --extensions porn gambling fakenews
