92384a7577d654d16077556c2a925a7515483aa1
BurningSmile <burningsmile10@gmail.com>
2017-05-28 19:14:54 -0600

    Updated vm script

--- Windows10Start.sh
+++ Windows10Start.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 #Load virtio
 sudo modprobe virtio
@@ -6,11 +6,14 @@ sudo modprobe virtio
 #Load uefi
 cp /usr/share/edk2.git/ovmf-x64/OVMF_VARS-pure-efi.fd /tmp/my_vars.fd
 
+# Load network bridge on eno1 interface
+sudo /home/david/.scripts/Virtual-Machines/qemu-bridge-ifup.sh
+
 #Start Vm
 qemu-system-x86_64 \
        -enable-kvm \
        -boot order=c \
-       -drive file=/home/USERNAMEHERE/virtual_machines/Windows10/Windows10disk,format=raw \
+       -drive file=/home/david/virtual-machines/Windows-10/Windows10disk,format=raw \
        -m 4G \
        -cpu host \
        -smp 4,cores=2,threads=2,sockets=1 \
@@ -23,5 +26,19 @@ qemu-system-x86_64 \
        -spice port=3001,disable-ticketing \
        -device virtio-serial \
        -chardev spicevmc,id=vdagent,debug=0,name=vdagent \
-       -device virtserialport,chardev=vdagent,name=com.redhat.spice.0
-       "$@"
+       -device virtserialport,chardev=vdagent,name=com.redhat.spice.0 \
+	-device ich9-usb-ehci1,id=usb \
+	-device ich9-usb-uhci1,masterbus=usb.0,firstport=0,multifunction=on \
+	-device ich9-usb-uhci2,masterbus=usb.0,firstport=2 \
+	-device ich9-usb-uhci3,masterbus=usb.0,firstport=4 \
+	-chardev spicevmc,name=usbredir,id=usbredirchardev1 \
+	-device usb-redir,chardev=usbredirchardev1,id=usbredirdev1 \
+	-chardev spicevmc,name=usbredir,id=usbredirchardev2 \
+	-device usb-redir,chardev=usbredirchardev2,id=usbredirdev2 \
+	-chardev spicevmc,name=usbredir,id=usbredirchardev3 \
+	-device usb-redir,chardev=usbredirchardev3,id=usbredirdev3 \
+        -net nic,macaddr=DE:AD:BE:EF:B8:78 -net bridge,br=bridge0 \
+	"$@"
+
+# Close network bridge
+sudo /home/david/.scripts/Virtual-Machines/qemu-bridge-ifdown.sh
