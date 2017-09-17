fa0b67aaf70b9c664f6ca7b8d21acc4c376d0780
BurningSmile <burningsmile10@gmail.com>
2017-09-16 21:57:23 -0600

    Updated install you-complete-me.sh

--- Arch/install-you-complete-me.sh
+++ Arch/install-you-complete-me.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-sudo pacman -S cmake clang python python3 --noconfirm
-mkdir ~/ycm_build
-cd ~/ycm_build
+sudo pacman -S --needed cmake clang python python3 --noconfirm
+mkdir /tmp/ycm_build
+cd /tmp/ycm_build
 cmake -G "Unix Makefiles" . ~/.vim/plugged/YouCompleteMe/third_party/ycmd/cpp
 cmake -G "Unix Makefiles" -DUSE_SYSTEM_LIBCLANG=ON . ~/.vim/plugged/YouCompleteMe/third_party/ycmd/cpp
 cmake --build . --target ycm_core --config Release
-cd ~
-rm -rf ~/ycm_build
