2087a43d3e2731873859a1421b0b9dcb126dc4ac
BurningSmile <burningsmile10@gmail.com>
2017-12-04 22:43:31 -0700

    removed old script

--- updatebudhud.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /tmp
-git clone https://github.com/rbjaxter/budhud.git
-cd ~
-rm -rf '/home/USERNAMEHERE/.local/share/Steam/steamapps/common/Team Fortress 2/tf/custom/budhud/'
-mv /tmp/budhud/budhud/ '/home/USERNAMEHERE/.local/share/Steam/steamapps/common/Team Fortress 2/tf/custom/'
